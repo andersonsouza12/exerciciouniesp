f6148f5574e8f9fcf656a4063325a86e8f2c57c4
Anderson <andersonfsouza12@gmail.com>
2020-09-26 22:20:25 -0300

    exercicio antigo

---
+++ src/exercicio/CalcualrDesconto.java
@@ -0,0 +1,24 @@
+package exercicio;
+
+public class CalcualrDesconto {
+
+	public static void main(String[] args) {
+		
+		int valorHr = 10;
+		int horasMes = 100;
+		
+		int salario = valorHr * horasMes;
+		int iprf = salario - (11 / 100);
+		int inss = salario - (8 / 100);
+		int sindi = salario - (5 / 100);
+		int salarioLiqui = salario - (iprf + inss + sindi);
+		int desconto = iprf + inss + sindi;
+		System.out.println("S�LARIO BRUTO: R$ " + salario);
+		System.out.println("IPRF: R$ " + iprf);
+		System.out.println("INSS: R$ " + inss);
+		System.out.println("SINDICATO: R$ " + sindi);
+		System.out.println("SAL�RIO LIQUIDO: R$ " + salarioLiqui);
+		System.out.println("VALOR DOS DESCONTOS: R$ " + desconto);
+	}
+
+}
